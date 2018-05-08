82f07fafbaf0eb6dd9df2b8621883daa92acbb05
zengke123 <249626102@qq.com>
2018-05-08 15:33:26 +0800

    init commit

---
+++ handlers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# 为文本块打上合适的 HTML 标记
+
+class Handler:
+    """
+    处理程序父类
+    """
+    def callback(self, prefix, name, *args):
+        method = getattr(self, prefix + name, None)
+        if callable(method): return method(*args)
+
+    def start(self, name):
+        self.callback('start_', name)
+
+    def end(self, name):
+        self.callback('end_', name)
+
+    def sub(self, name):
+        def substitution(match):
+            result = self.callback('sub_', name, match)
+            if result is None: result = match.group(0)
+            return result
+        return substitution
+
+class HTMLRenderer(Handler):
+    """
+    HTML 处理程序,给文本块加相应的 HTML 标记
+    """
+
+    pass
+
---
+++ util.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# 实现文本块生成器把纯文本分成一个一个的文本块，以便接下来对每一个文本块进行解析
+
+def lines(file):
+    """
+    生成器,在文本最后加一空行
+    """
+    for line in file: yield line
+    yield '\n'
+
+def blocks(file):
+    """
+    生成器,生成单独的文本块
+    以<end>标签作为段落分隔标志
+
+    """
+    block = []
+    for line in lines(file):
+        if not line.startswith("<end>"):
+            block.append(line)
+        elif block:
+            yield ''.join(block).strip()
+            block = []
+
