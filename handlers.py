c18f32f9874d20771f3d0c17645b899bc8d53121
zengke123 <249626102@qq.com>
2018-05-12 01:49:15 +0800

    add handlers

--- handlers.py
+++ handlers.py
@@ -9,72 +9,111 @@ class Handler:
         method = getattr(self, prefix + name, None)
         if callable(method): return method(*args)
 
-    def start(self, name):
-        self.callback('start_', name)
+    def start(self, name, *args):
+        self.callback('start_', name, *args)
 
-    def end(self, name):
-        self.callback('end_', name)
+    def end(self, name, *args):
+        self.callback('end_', name, *args)
 
-    def sub(self, name):
-        def substitution(match):
-            result = self.callback('sub_', name, match)
-            if result is None: result = match.group(0)
-            return result
-        return substitution
 
 class HTMLRenderer(Handler):
     """
     HTML 处理程序,给文本块加相应的 HTML 标记
     """
-    def start_document(self):
-        print('<html><head><title>例检报告</title></head><body>')
 
-    def end_document(self):
-        print('</body></html>')
+    def start_title_h1(self, title):
+        print('<div class="report_h report_h1">' + str(title) + '</div>\n<div class="report_content">')
 
-    def start_paragraph(self):
-        print('<p style="color: #444;">')
+    def end_title_h1(self):
+        print('</div>')
 
-    def end_paragraph(self):
-        print('</p>')
+    def start_title_h2(self,title):
+        print('<div class="report_h report_h2">'+ str(title) + '</div>\n<div>')
 
-    def start_heading(self):
-        print('<h2 style="color: #68BE5D;">')
+    def end_title_h2(self):
+        print('</div>')
 
-    def end_heading(self):
-        print('</h2>')
+    def start_tr(self,css_name):
+        # css_name in (even,odd,second_title)
+        print('<tr class="'+ str(css_name) + '">')
 
-    def start_list(self):
-        print('<ul style="color: #363736;">')
+    def end_tr(self):
+        print('</tr>')
 
-    def end_list(self):
-        print('</ul>')
+    # def start_th(self, width):
+    #     print('<th width=' + str(width)  + '>')
 
-    def start_listitem(self):
-        print('<li>')
+    def start_th(self):
+        print('<th>')
 
-    def end_listitem(self):
-        print('</li>')
+    def end_th(self):
+        print('</th>')
 
-    def start_title(self):
-        print('<h1 style="color: #1ABC9C;">')
+    def start_td(self):
+        print('<td>')
 
-    def end_title(self):
-        print('</h1>')
+    def end_td(self):
+        print('</td>')
 
-    def sub_emphasis(self, match):
-        return '<em>%s</em>' % match.group(1)
+    def start_table(self):
+        print('<table class="report_table">')
 
-    def sub_url(self, match):
-        return '<a target="_blank" style="text-decoration: none;color: #BC1A4B;" href="%s">%s</a>' % (
-        match.group(1), match.group(1))
-
-    def sub_mail(self, match):
-        return '<a style="text-decoration: none;color: #BC1A4B;" href="mailto:%s">%s</a>' % (
-        match.group(1), match.group(1))
+    def end_table(self):
+        print('</table>')
 
     def feed(self, data):
         print(data)
 
 
 
+    def start_head(self):
+        header = '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+        <title>主机报表</title>
+        <link rel="stylesheet" href="media/report/css/ns_report.css" />
+        <link rel="stylesheet" href="media/report/css/ns_report_rsas.css" />
+        <script src="media/report/js/jquery-1.7.2.js"></script>
+        <script src="media/report/js/common.js"></script>
+        </head>
+        <body>
+	
+        <div id="report" class="wrapper">
+          <!--div class="report_tip"></div-->
+          <div id="head" class="report_title">
+            <h1>例检报告-AS381</h1>
+            <span class="note">&nbsp;</span> 
+          </div>
+          <!--head end,catalog start-->
+          <div id="catalog">
+            <div class="report_h1">目录</div>
+          </div>
+          <div id="content">
+        '''
+        print(header)
+
+    def end_head(self):
+        end_header = '''
+        <script type="text/javascript">
+	    function getPageY(element){
+	  	    return element.offsetTop + (element.offsetParent ? arguments.callee(element.offsetParent) : 0) 
+	    }
+	    jQuery(function($){
+            //window.dialog = new UI.Dialog({name:'dialog'});
+            $("#catalog_tree").report_tree();
+            $("#report").catalog();
+            $('#catalog').delegate('a.link', 'click', function(event){
+                event.preventDefault();
+                $("#catalog").find("div.report_content").hide();
+                $("#catalog").find(".h1_dot").addClass("up");
+                jumpToHash(event.target.hash);
+            });
+            $.gotop();
+	    });
+        </script>
+        </body>
+        </html>
+        '''
+        print(end_header)
