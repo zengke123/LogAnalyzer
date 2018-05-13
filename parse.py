40fb258423e8ddbbfbcee56032737a4ccc73f495
zengke123 <249626102@qq.com>
2018-05-14 00:18:57 +0800

    添加主程序

--- parse.py
+++ parse.py
@@ -1,10 +1,11 @@
-# 主程序，对文本进行解析
+#!/usr/bin/env python
 # encoding: utf-8
-import sys
+# 主程序，对文本进行解析,并输出到对应的文件
+import os
 from handlers import *
 from util import *
 from rules import *
-
+from logger import *
 
 class Parser:
     """
@@ -20,11 +21,11 @@ class Parser:
         """
         self.rules.append(rule)
 
-    def parse(self, file):
+    def parse(self, file, hostname):
         """
         解析
         """
-        self.handler.start('head')
+        self.handler.start('head',hostname)
         first_title_h1 = 0
         for block in blocks(file):
             for rule in self.rules:
@@ -50,25 +51,18 @@ class LogParser(Parser):
         self.addRule(TableRule())
         self.addRule(MixTableRule())
 
-class Logger():
-    def __init__(self, filename="Default.log"):
-        self.terminal = sys.stdout
-        self.log = open(filename, "a")
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-
-    def flush(self):
-        pass
-
 if __name__ == '__main__':
-    file = "log/AS381.201805071050.txt"
-    # sys.stdout = Logger("output/host/AS381.html")
-    with open(file, 'r') as f:
-        handler = HTMLRenderer()
-        parser = LogParser(handler)
-        parser.parse(f)
-
-
+    # file1 = "AS381.201805071050.txt"
+    # file2 = "AS382.201805071050 - 副本.txt"
+    LogPath = "log" + os.sep
+    HtmlPath = "output" + os.sep + "host" + os.sep
+    FileList = os.listdir(LogPath)
+    handler = HTMLRenderer()
+    parser = LogParser(handler)
+    for file in FileList:
+        hostname = file.split('.')[0]
+        output = HtmlPath + hostname + ".html"
+        # 开始解析
+        with LogSave(output):
+            with open(LogPath + file, 'r') as f:
+                parser.parse(f, hostname)
